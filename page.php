b41314eeebbe97469a6897053c0478a13c85010a
Raiyan Aaijaz <raiyanjaz@gmail.com>
2023-06-01 16:53:21 -0400

    updated

--- page.php
+++ page.php
@@ -1,3 +1,16 @@
 <?php get_header(); ?>
 
+<!--
+<div class="page">
+    <div class="page-banner">
+        <p><?php the_title(); ?></p>
+    </div>
+    <div class="page-body">
+        <div class="page-content">
+            <p><?php the_content(); ?></p>
+        </div>
+        <img src="<?php echo get_theme_file_uri('/images/lakeSunrise.jpg')?>">
+    </div>
+</div> --->
+
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -4,16 +4,16 @@ while (have_posts()) {
     the_post();
 } ?>
 
-<div class="page-body">
-    <div class="page-header">
-        <div class="page-title">
+<div class="single-body">
+    <div class="single-header">
+        <div class="single-title">
             <span><?php the_title(); ?></span>
         </div>
         <img src="<?php 
         if (get_the_ID(the_post()) == 28)        
             echo get_theme_file_uri('images/canadaFlag.jpg'); ?>">
     </div>
-    <div class="page-content">
+    <div class="single-content">
         <span><?php the_content(); ?></span>
     </div>
     <div class=" register-btn">
