c3dd1da3b4985ec67f00e7c41d4f4eae15c13753
Raiyan Aaijaz <raiyanjaz@gmail.com>
2023-06-06 18:17:59 -0400

    Updated services page

--- page.php
+++ page.php
@@ -5,45 +5,39 @@
 </div>
 
 <?php 
-if (get_the_ID(the_post()) == 33) {
+if (get_the_ID(the_post()) == 33) { // If on the service page
     $courses = new WP_Query(array(
         'post_type' => 'courses'
     ));          
+    
+    $counter = 0; // Initialize counter variable
+
+    while ($courses->have_posts()) {
+        $courses->the_post();
+        
+        if ($counter % 4 === 0) { // Create a new course container every fourth post
+            if ($counter > 0) {
+                echo '</div>'; // Close the previous course container
+            }
+            echo '<div class="course-container">';
+        }
 ?>
 
-<?php
-        while ($courses->have_posts()) {
-            for ($i = 0; $i < 4 && $courses->have_posts(); $i++) {     
-?>
-<div class="course-container">
-    <?php
-                for ($j = 0; $j < 4 && $courses->have_posts(); $j++) {
-                    $courses->the_post();
-    ?>
-    <div class="card">
-        <img src="<?php echo get_theme_file_uri('/images/lakeSunrise.jpg')?>" alt="Course 1">
-        <h2><?php echo wp_trim_words(get_the_title(), 3); ?></h2>
-        <p><?php echo wp_trim_words(get_the_content(), 20); ?></p>
-    </div>
-    <?php       
-if ($courses->have_post() == false) {
-    break;
-}   
-} 
-    ?>
+<div class="card">
+    <img src="<?php echo get_theme_file_uri('/images/lakeSunrise.jpg')?>" alt="Course 1">
+    <h2><?php echo wp_trim_words(get_the_title(), 3); ?></h2>
+    <p><?php echo wp_trim_words(get_the_content(), 20); ?></p>
 </div>
-<?php   }?>
-<?php 
-        } 
-    ?>
 
-<?php
+<?php       
+        $counter++;
+
+        if ($courses->current_post + 1 === $courses->post_count || $counter % 4 === 0) {
+            echo '</div>'; // Close the last course container
+        }
     }
+}
 ?>
-
-
-
-
 
 <!--
 <div class="page">
