e31f89ef5da6076d0fcb1f4b4d5c52ebeec48906
Raiyan Aaijaz <raiyanjaz@gmail.com>
2023-06-06 20:03:19 -0400

    Completed frontend services page

--- footer.php
+++ footer.php
@@ -1,5 +1,28 @@
-<footer class="main-footer">
-    <span>© 2023 Raireen Learning Centre. All Rights Reserved.</span>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<footer>
+    <div class="footer">
+        <div class="row">
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-instagram"></i></a>
+            <a href="#"><i class="fa fa-youtube"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+        </div>
+
+        <div class="row">
+            <ul>
+                <li><a href="#">Contact us</a></li>
+                <li><a href="#">Our Services</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms & Conditions</a></li>
+                <li><a href="#">Career</a></li>
+            </ul>
+        </div>
+
+        <div class="row">
+            © <?php echo date('Y'); ?> Raireen Learning Centre. All Rights Reserved.
+        </div>
+    </div>
 </footer>
 
 <?php wp_footer(); ?>
